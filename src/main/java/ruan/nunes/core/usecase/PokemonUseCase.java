ef2422c0323c79bbd8f24c7a2d9b55e3b09b2329
RuanNunes <ruanschwanz@gmail.com>
2023-06-11 20:18:17 -0300

    init implements clean arc

--- src/main/java/ruan/nunes/core/usecase/PokemonUseCase.java
+++ src/main/java/ruan/nunes/core/usecase/PokemonUseCase.java
@@ -1,30 +1,20 @@
-package ruan.nunes.api;
+package ruan.nunes.core.usecase;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import ruan.nunes.data.PokemonEntity;
 import ruan.nunes.data.client.PokemonClient;
-import ruan.nunes.data.client.dto.PokemonList;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/pokemon")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
-public class PokemonResource {
+@ApplicationScoped
+public class PokemonUseCase {
     @Inject
     @RestClient
     PokemonClient pokemonClient;
-
-    @GET
-    @Path("/client")
-    public PokemonList pokemonList(){
+    public List<PokemonEntity> migratePokemons(){
         final var response = pokemonClient.getPokemons(0,2000);
         final List<PokemonEntity> entityList = new ArrayList<>();
         response.results().forEach(r -> {
@@ -35,7 +25,6 @@ public class PokemonResource {
             entityList.add(entity);
         });
         PokemonEntity.persist(entityList);
-        return response;
+        return (List<PokemonEntity>) PokemonEntity.findAll();
     }
-
 }
--- src/main/java/ruan/nunes/data/PokemonEntity.java
+++ src/main/java/ruan/nunes/data/PokemonEntity.java
@@ -2,10 +2,14 @@ package ruan.nunes.data;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import lombok.Builder;
+import lombok.*;
 
+@EqualsAndHashCode(callSuper = true)
 @MongoEntity(collection = "pokemon",database = "pokemon-db")
 @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PokemonEntity extends PanacheMongoEntity {
     private String name;
     private String url;
