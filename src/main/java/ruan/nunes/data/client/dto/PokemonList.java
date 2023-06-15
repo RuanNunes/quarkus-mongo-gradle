ba9b15b309b50cea2d9713d74a1d309cb6eec8da
RuanNunes <ruanschwanz@gmail.com>
2023-06-15 18:42:49 -0300

    push

--- src/main/java/ruan/nunes/data/client/dto/PokemonList.java
+++ src/main/java/ruan/nunes/data/client/dto/PokemonList.java
@@ -1,5 +1,8 @@
 package ruan.nunes.data.client.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record PokemonList(Long count, String next, String previous, List<Results> results){}
